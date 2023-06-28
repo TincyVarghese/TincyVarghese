c4f26db3d668316c1cbbdaf9850597156e9302e2
TincyVarghese <124864694+TincyVarghese@users.noreply.github.com>
2023-06-28 10:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " One stop solution.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Products.html'>Products</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
